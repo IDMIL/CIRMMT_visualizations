0fd87855bd8ddf665177e7f27fa79b08b86940b9
Mathias Bredholt <mathiasbredholt@gmail.com>
2019-05-29 14:33:42 -0400

    added sidebar, keyword links

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,26 @@ const path = require('path');
 
 module.exports = {
     entry: './src/index.js',
-    mode: 'production',
+    mode: 'development',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
     },
     module: {
         rules: [{
-            test: /\.(csv|tsv)$/,
-            loader: 'csv-loader',
-            options: {
-                header: true
+                test: /\.(csv|tsv)$/,
+                loader: 'csv-loader',
+                options: {
+                    header: true
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
             }
-        }]
+        ]
     }
 };
