6001cfdfd182294eaaeeb978c179f08f1332f3b2
Mathias Bredholt <mathiasbredholt@gmail.com>
2019-05-30 19:06:01 -0400

    fixed text wrapping and adjusted layout

--- src/KeywordView.js
+++ src/KeywordView.js
@@ -5,19 +5,24 @@ const width = 700;
 const height = 700;
 
 function showKeywordView(clicked) {
-    let list = document.getElementsByClassName("graph");
+    let list = document.getElementsByClassName('graph');
     while (list[0]) {
         list[0].parentNode.removeChild(list[0]);
     }
 
-    let color = d3.scaleOrdinal().range(["LightCyan", "LightGreen"]).domain([40, 60]);
+    let color = d3.scaleOrdinal().range(["#fbb4ae", "#b3cde3", "#ccebc5", "#decbe4", "#fed9a6", "#ffffcc", "#e5d8bd", "#fddaec", "#f2f2f2"]).domain([40, 60]);
 
     let nodes = {};
     let links = [];
 
     data.forEach(function(d) {
         nodes[d.MainLevel] = { id: d.MainLevel, value: 60 };
-        nodes[d.Level1] = { id: d.Level1, value: 40 };
+        if (nodes[d.Level1] != undefined) {
+            nodes[d.Level1].value += 3;
+        } else {
+            nodes[d.Level1] = { id: d.Level1, value: 35 };
+        }
+
         let l = {};
         l.source = d.MainLevel;
         l.target = d.Level1;
@@ -26,61 +31,65 @@ function showKeywordView(clicked) {
 
     nodes = Object.values(nodes);
 
-    // console.log(links);
+    console.log(nodes);
 
     let simulation = d3.forceSimulation(nodes)
-        .force("link", d3.forceLink(links).id(d => d.id).distance(50))
-        .force("charge", d3.forceManyBody().strength(-400))
-        .force("center", d3.forceCenter(width / 2, height / 2))
-        .force("collide", d3.forceCollide(60).strength(1));
+        .force('link', d3.forceLink(links).id(d => d.id).distance(50))
+        .force('charge', d3.forceManyBody().strength(-400))
+        .force('center', d3.forceCenter(width / 2, height / 2))
+        .force('collide', d3.forceCollide(60).strength(1));
 
 
-    let svg = d3.select("#container").append("svg")
-        // .attr("width", width)
-        // .attr("height", height)
-        .attr("viewBox", `0 0 ${width} ${height}`)
-        // .attr("preserveAspectRatio", "xMidYMid")
-        .attr("class", "graph");
+    let svg = d3.select('#container').append('svg')
+        // .attr('width', width)
+        // .attr('height', height)
+        .attr('viewBox', `0 0 ${width} ${height}`)
+        // .attr('preserveAspectRatio', 'xMidYMid')
+        .attr('class', 'graph');
 
-    let link = svg.append("g")
-        .attr("stroke", "Thistle")
-        .selectAll("line")
+    let link = svg.append('g')
+        .attr('stroke', '#CCC')
+        .selectAll('line')
         .data(links)
-        .join("line")
-        .attr("stroke-width", 4);
+        .join('line')
+        .attr('stroke-width', 4);
 
-    let node = svg.append("g")
-        .selectAll("g")
+    let node = svg.append('g')
+        .selectAll('g')
         .data(nodes)
-        .join("g")
-        .attr("class", "node")
-        .attr("transform", d => "translate(" + d.x + "," + d.y + ")");
+        .join('g')
+        .attr('class', 'node')
+        .attr('transform', d => 'translate(' + d.x + ',' + d.y + ')');
 
-    node.append("circle")
-        .attr("stroke", "Thistle")
-        .attr("stroke-width", 4)
-        .attr("r", d => d.value)
-        .attr("fill", d => color(d.value));
+    node.append('circle')
+        .attr('stroke', '#CCC')
+        .attr('stroke-width', 4)
+        .attr('r', d => d.value)
+        .attr('fill', d => color(d.value));
 
-    node.append("text")
-        .text(d => d.id)
-        .attr("text-anchor", "middle")
-        .attr("class", "nodeText");
+    node.append('foreignObject')
+        .attr('class', 'nodeTextBox')
+        .attr('x', d => -d.value)
+        .attr('y', d => -d.value)
+        .attr('width', d => d.value * 2)
+        .attr('height', d => d.value * 2)
+        .append('xhtml:body')
+        .attr('class', 'nodeTextBoxBody')
+        .html(d => d.id);
 
-    simulation.on("tick", () => {
+    simulation.on('tick', () => {
         link
-            .attr("x1", d => d.source.x)
-            .attr("y1", d => d.source.y)
-            .attr("x2", d => d.target.x)
-            .attr("y2", d => d.target.y);
+            .attr('x1', d => d.source.x)
+            .attr('y1', d => d.source.y)
+            .attr('x2', d => d.target.x)
+            .attr('y2', d => d.target.y);
         node
-            .attr("transform", d => "translate(" + d.x + "," + d.y + ")");
+            .attr('transform', d => 'translate(' + d.x + ',' + d.y + ')');
     });
 
-    node.on("click", d => {
+    node.on('click', d => {
         clicked(d.id);
     });
-    // d3.selectAll("foreignObject").attr("x", -38).attr("y", -20);
 };
 
 export default showKeywordView;
--- src/VideoView.js
+++ src/VideoView.js
@@ -5,12 +5,12 @@ const width = 700;
 const height = 700;
 
 function showVideoView(selectedNode, videoClicked, keywordNodeClicked) {
-    let list = document.getElementsByClassName("graph");
+    let list = document.getElementsByClassName('graph');
     while (list[0]) {
         list[0].parentNode.removeChild(list[0]);
     }
 
-    let color = d3.scaleOrdinal().range(["LightCyan", "LightGreen"]).domain([40, 60]);
+    let color = d3.scaleOrdinal().range(["#fbb4ae", "#b3cde3", "#ccebc5", "#decbe4", "#fed9a6", "#ffffcc", "#e5d8bd", "#fddaec", "#f2f2f2"]).domain([40, 60]);
 
     let nodes = {};
     let links = [];
@@ -36,79 +36,112 @@ function showVideoView(selectedNode, videoClicked, keywordNodeClicked) {
 
     console.log(nodes);
 
-
     let simulation = d3.forceSimulation(nodes)
-        .force("link", d3.forceLink(links).id(d => d.id).distance(200))
-        .force("charge", d3.forceManyBody().strength(-2000))
-        .force("center", d3.forceCenter(width / 2, height / 2))
-        .force("collide", d3.forceCollide(60).strength(0.4));
+        .force('link', d3.forceLink(links).id(d => d.id).distance(250))
+        .force('charge', d3.forceManyBody().strength(-2000))
+        .force('center', d3.forceCenter(width / 3, height / 2))
+        .force('collide', d3.forceCollide(60).strength(0.4));
 
-    let svg = d3.select("#container").append("svg")
-        .attr("width", width)
-        .attr("height", height)
-        .attr("viewBox", `0 0 ${width} ${height}`)
-        .attr("class", "graph");
+    let svg = d3.select('#container').append('svg')
+        .attr('width', width)
+        .attr('height', height)
+        .attr('viewBox', `0 0 ${width} ${height}`)
+        .attr('class', 'graph');
 
-    let link = svg.append("g")
-        .attr("stroke", "Thistle")
-        .selectAll("line")
+    let link = svg.append('g')
+        .attr('stroke', '#CCC')
+        .selectAll('line')
         .data(links)
-        .join("line")
-        .attr("stroke-width", 4);
+        .join('line')
+        .attr('stroke-width', 4);
 
-    let node = svg.append("g")
-        .selectAll("g")
+    let node = svg.append('g')
+        .selectAll('g')
         .data(nodes)
-        .join("g")
-        .attr("class", "node")
-        .attr("transform", d => "translate(" + d.x + "," + d.y + ")");
+        .join('g')
+        .attr('class', 'node')
+        .attr('transform', d => 'translate(' + d.x + ',' + d.y + ')');
 
-    node.append("circle")
-        .attr("stroke", "Thistle")
-        .attr("stroke-width", 4)
-        .attr("r", d => d.value)
-        .attr("fill", d => color(d.value));
+    node.append('circle')
+        .attr('stroke', '#CCC')
+        .attr('stroke-width', 4)
+        .attr('r', d => d.value)
+        .attr('fill', d => color(d.value));
 
-    let title = node.append("text")
-        .text(d => {
+    // let title = node.append('text')
+    //     .text(d => {
+    //         if (d.Lecturer) {
+    //             return d.Lecturer;
+    //         } else {
+    //             return d.id;
+    //         }
+    //     })
+    //     .attr('class', 'nodeText')
+    //     .attr('text-anchor', d => {
+    //         if (d.Lecturer) {
+    //             return 'left';
+    //         } else {
+    //             return 'middle';
+    //         }
+    //     });
+
+
+    node.append('foreignObject')
+        .attr('class', d => {
+            if (d.Lecturer) {
+                return 'noBox';
+            } else {
+                return 'nodeTextBox';
+            }
+        })
+        .attr('x', d => -d.value)
+        .attr('y', d => -d.value)
+        .attr('width', d => d.value * 2)
+        .attr('height', d => d.value * 2)
+        .append('xhtml:body')
+        .attr('class', 'nodeTextBoxBody')
+        .html(d => d.id);
+
+    let title = node.append('foreignObject')
+        .attr('class', d => {
+            if (d.Lecturer) {
+                return 'nodeTitleBox';
+            } else {
+                return 'noBox';
+            }
+        })
+        .attr('x', d => d.value + 8)
+        .attr('y', d => -d.value)
+        .attr('width', 150)
+        .attr('height', 200);
+
+    title.append('xhtml:div')
+        .attr('class', 'nodeLecturer')
+        .html(d => {
             if (d.Lecturer) {
                 return d.Lecturer;
-            } else {
-                return d.id;
-            }
-        })
-        .attr("class", "nodeText")
-        .attr("text-anchor", d => {
-            if (d.Lecturer) {
-                return "left";
-            } else {
-                return "middle";
             }
         });
-    // .attr("transform", d => "translate(" + (d.value + 4) + "," + 3 + ")");
 
-
-    title.append("tspan")
-        .text(d => {
+    title.append('xhtml:div')
+        .attr('class', 'nodeTitle')
+        .html(d => {
             if (d.Lecturer) {
                 return d.id;
             }
-        })
-        .attr("class", "nodeText")
-        .attr("x", 0)
-        .attr("dy", "1em");
+        });
 
-    simulation.on("tick", () => {
+    simulation.on('tick', () => {
         link
-            .attr("x1", d => d.source.x)
-            .attr("y1", d => d.source.y)
-            .attr("x2", d => d.target.x)
-            .attr("y2", d => d.target.y);
+            .attr('x1', d => d.source.x)
+            .attr('y1', d => d.source.y)
+            .attr('x2', d => d.target.x)
+            .attr('y2', d => d.target.y);
         node
-            .attr("transform", d => "translate(" + d.x + "," + d.y + ")");
+            .attr('transform', d => 'translate(' + d.x + ',' + d.y + ')');
     });
 
-    node.on("click", d => {
+    node.on('click', d => {
         if (d.Lecturer) {
             videoClicked(d);
         } else {
